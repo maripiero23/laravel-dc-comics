32b87f7296e89a6cd87ad9e4723dc24f4739a4d5
maripiero23 <mari.pieropan@gmail.com>
2023-01-26 19:49:48 +0100

    da modifucare le colonne

---
+++ database/migrations/2023_01_26_183232_update_column_to_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comics', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('description');
+            $table->string('thumb');
+            $table->float('price', 5, 2);
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comics', function (Blueprint $table) {
+            //
+        });
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ComicsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/comics', [ComicsController::class, 'index'])->name('comics.index');
+
+Route::get('/comics/show', [ComicsController::class, 'show'])->name('comics.show');
+
+Route::get('/comics/create', [ComicsController::class, 'create'])->name('comics.create');
+
+Route::get('/comics', [ComicsController::class, 'store'])->name('comics.store');
+
